918ef83373155b7048ee0c0ae80ad3d7d2e1fe3a
Khan <imamul.khan@openreach.co.uk>
2024-01-21 21:11:54 +0530

    Added

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import ExpenseTracker from './expenseTracer';
+function App() {
+  return (
+   <ExpenseTracker />
+  );
+}
+
+export default App;
